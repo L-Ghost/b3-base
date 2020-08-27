29fbbe50e783cc5719a68990f825ae53f24b8cda
L-Ghost <luis.arantes.sp@gmail.com>
2020-08-26 22:07:24 -0300

    add common and preferred share type filtering

--- b3/stocks_list.py
+++ b3/stocks_list.py
@@ -1,10 +1,25 @@
+import re
+
 
 class StocksList:
+    COMMON_SHARES_REGEX = "^\w{4}3$"
+    PREFERRED_SHARES_REGEX = "^\w{4}4$"
+
     def __init__(self, stocks):
         self.stocks = stocks
 
     def tickers(self):
         return [stock_info[1] for stock_info in self.stocks]
+
+    def common_shares(self):
+        return list(filter(
+            lambda ticker: self.__common_share_ticker(ticker), self.tickers()
+        ))
+
+    def preferred_shares(self):
+        return list(filter(
+            lambda ticker: self.__preferred_share_ticker(ticker), self.tickers()
+        ))
 
     def to_dict(self):
         return {
@@ -15,4 +30,10 @@ class StocksList:
                 'last_price': stock_info[7]
             } for stock_info in self.stocks
         }
+
+    def __common_share_ticker(self, ticker):
+        return re.search(self.COMMON_SHARES_REGEX, ticker)
+
+    def __preferred_share_ticker(self, ticker):
+        return re.search(self.PREFERRED_SHARES_REGEX, ticker)
 
--- tests/b3/stocks_list_test.py
+++ tests/b3/stocks_list_test.py
@@ -5,17 +5,34 @@ from b3 import StocksList
 @pytest.fixture
 def stocks_list():
     '''Retrieves a list with stocks data'''
-    first_stock = [
+    trade_information_stocks = [
+        [
             '2020-07-24', 'PETR3', 'BRPETRACNOR9', 'CASH',
             '23,05', '23,69', '23,4', '23,33', '', '', '21458', '11202000'
-        ]
-    second_stock = [
+        ],
+        [
             '2020-07-24', 'PETR4', 'BRPETRACNPR6', 'CASH',
             '22,42', '23,06', '22,75', '22,73', '', '', '45405', '43380300'
         ]
-    trade_information_stocks = [first_stock, second_stock]
+    ]
 
     return StocksList(trade_information_stocks)
+
+@pytest.fixture
+def stocks_tickers_list():
+    '''Retrieves a list with tickers stocks data only'''
+    return StocksList([
+        ['2020-08-26', 'PETR3'],
+        ['2020-08-26', 'PETR4'],
+        ['2020-08-26', 'BBAS3'],
+        ['2020-08-26', 'ELET3'],
+        ['2020-08-26', 'ELET6'],
+        ['2020-08-26', 'KLBN4'],
+        ['2020-08-26', 'KLBN11'],
+        ['2020-08-26', 'SNSY5'],
+        ['2020-08-26', 'AMZO34'],
+    ])
+
 
 def test_parse_list_into_dict(stocks_list):
     assert stocks_list.to_dict() == {
@@ -35,4 +52,10 @@ def test_parse_list_into_dict(stocks_list):
 
 def test_retrieve_tickers_from_list(stocks_list):
     assert stocks_list.tickers() == ['PETR3', 'PETR4']
+
+def test_retrieve_common_shares(stocks_tickers_list):
+    assert stocks_tickers_list.common_shares() == ['PETR3', 'BBAS3', 'ELET3']
+
+def test_retrieve_preferred_shares(stocks_tickers_list):
+    assert stocks_tickers_list.preferred_shares() == ['PETR4', 'KLBN4']
 
